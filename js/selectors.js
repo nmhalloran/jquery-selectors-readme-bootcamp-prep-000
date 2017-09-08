9e6941b71345aa4fa42196ad9001df6167f3245f
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-08 21:18:55 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return($('p'))
+}
+
+function lastImageSelector() {
+  return($('img:last'))
+}
+
+function ninjaBabySelector() {
+  return("img[id='baby-ninja']")
+}
+
+function divSelector() {
+  
+}
+
+function firstListItem() {
+  return($('ul#pic-list li:first-child'))
+}
